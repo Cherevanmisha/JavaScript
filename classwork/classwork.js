e39a048be41a208b78aaaa7057e27e18ee38d96f
Cherevanmisha <cherevanmisha@ukr.net>
2021-11-11 20:11:47 +0200

    Classwork

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,10 +1,99 @@
-// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor).
+// Скласти результат цих чисел в змінній result.
+// let array = [1, 10, 15, 2, 7, 5, 66, 11, 21, 70];
+// let result = array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6] + array[7] + array[8] + array[9];
+// console.log(result)
+
+
+
 // 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+// let book = [
+//     {
+//         title: 'Harry Potter and the Philosophers Stone',
+//         author: 'J.K. Rowling',
+//         numberOfPages: 500,
+//         genre: 'fantasy',
+//     },
+//     {
+//         title: 'Steve Jobs',
+//         author: 'Walter Isaacson',
+//         numberOfPages: 300,
+//         genre: 'Biography',
+//     },
+//     {
+//         title: 'War and Peace',
+//         author: 'Lev Tolstoy',
+//         numberOfPages: 1200,
+//         genre:'novel epic',
+//     }];
+
+
 // 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
-// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
-// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// let book = [
+//     {
+//         title: 'Harry Potter and the Philosophers Stone',
+//         author: 'J.K. Rowling',
+//         numberOfPages: 500,
+//         genre: 'fantasy',
+//     },
+//     {
+//         title: 'Steve Jobs',
+//         author: 'Walter Isaacson',
+//         numberOfPages: 300,
+//         genre: 'Biography',
+//     },
+//     {
+//         title: 'War and Peace',
+//         author: 'Lev Tolstoy',
+//         numberOfPages: 1200,
+//         genre:'novel epic',
+//     }];
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори.
+// Вивести кожну книгу як окремий об'єкт
+//
+// let book1 =
+//     {
+//         title: 'Harry Potter and the Philosophers Stone',
+//         author: 'J.K. Rowling',
+//         numberOfPages: 500,
+//         genre: 'fantasy',
+//     };
+//     let book2 = {
+//         title: 'Steve Jobs',
+//         author: 'Walter Isaacson',
+//         numberOfPages: 300,
+//         genre: 'Biography',
+//     };
+//     let book3 = {
+//         title: 'War and Peace',
+//         author: 'Lev Tolstoy',
+//         numberOfPages: 1200,
+//         genre:'novel epic',
+//     };
+//     console.log(book1,book2,book3)
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см.
+// (змінна height) та шириною 10см (змінна width).
 // Значення площі зберігати в змінній s.
+// let height = 23;
+// let width = 10;
+// let s = height * width;
+// console.log(s)
+
+
 // 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
 // результат помістіть у змінну v.
+// let heightC = 10;
+// let dC = 4;
+// let v = 3.14 * dC * heightC;
+// console.log(v);
+
 // 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
-// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow
+// (число, ступінь) або оператор зведення в ступінь **).
+//
+// let n = 3;
+// let m = 4;
+// let k = Math.pow(3, 2) + Math.pow(4, 2);
+// console.log(k)
